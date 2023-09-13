d0f76bed0df03bd889250d60f9f7686212b78b52
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-09-12 23:24:01 -0600

    Refactor NavDrawer with improved types

---
+++ src/components/Icons/PCIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function PCIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 21 16"
+      width="24"
+      height="24"
+    >
+      <path
+        d="M0 13.772l6.545.902V8.426H0zM0 7.62h6.545V1.296L0 2.198zm7.265 7.15l8.704 1.2V8.425H7.265zm0-13.57v6.42h8.704V0z"
+        fill="white"
+      ></path>
+    </svg>
+  )
+}
