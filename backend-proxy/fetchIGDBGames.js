667ef58f0313bbdfc961202b76dee0542de08260
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-10-06 13:08:03 -0600

    Add retry logic to fetch request

--- backend-proxy/fetchIGDBGames.js
+++ backend-proxy/fetchIGDBGames.js
@@ -2,26 +2,36 @@ const axios = require('axios')
 const { getIGDBAccessToken } = require('./auth.js')
 const { validateIGDBResponse } = require('./errorHandlers/validateFunctions.js')
 
-async function fetchIGDBGames(data) {
-  const accessToken = await getIGDBAccessToken()
-  const response = await axios({
-    method: 'post',
-    url: 'https://api.igdb.com/v4/games',
-    headers: {
-      Accept: 'application/json',
-      'Client-ID': process.env.IGDB_CLIENT_ID,
-      Authorization: `Bearer ${accessToken}`
-    },
-    data: data
-  })
+async function fetchIGDBGames(data, retries = 3) {
+  try {
+    const accessToken = await getIGDBAccessToken()
+    const response = await axios({
+      method: 'post',
+      url: 'https://api.igdb.com/v4/games',
+      headers: {
+        Accept: 'application/json',
+        'Client-ID': process.env.IGDB_CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`
+      },
+      data: data
+    })
 
-  // Log the headers from the IGDB API response
-  console.log('IGDB API Headers:', response.headers)
+    // Log the headers from the IGDB API response
+    console.log('IGDB API Headers:', response.headers)
 
-  console.log('IGDB response received:', response.data)
-  validateIGDBResponse(response)
-  console.log('IGDB response validated successfully')
-  return response.data
+    console.log('IGDB response received:', response.data)
+    validateIGDBResponse(response)
+    console.log('IGDB response validated successfully')
+    return response.data
+  } catch (error) {
+    if (error.response && error.response.status === 503 && retries > 0) {
+      console.log(`Retrying fetchIGDBGames... attempts left: ${retries - 1}`)
+      await new Promise((res) => setTimeout(res, 500))
+      return fetchIGDBGames(data, retries - 1)
+    } else {
+      throw error
+    }
+  }
 }
 
 module.exports = fetchIGDBGames
